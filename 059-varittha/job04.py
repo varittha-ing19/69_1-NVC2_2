3987eba6bc08a6ab193a6153508fa51357516ab2
nammon <khanmareingsmbasr@gmail.com>
2026-08-27 14:47:35 +0700

    1

---
+++ 059-varittha/job04.py
@@ -0,0 +1,17 @@
+student1 = {
+    "name": "Phakhamon Youngcharoon",
+    "age": 16,
+    "class": "2_2",
+    "phone": "0985876385"
+}
+
+student2 = {
+    "name": "Srinamphueng Wongngam",
+    "age": 16,
+    "class": "2_2",
+    "phone": "0851234567"
+}
+
+class2_2 = [student1, student2]
+
+print(class2_2)
