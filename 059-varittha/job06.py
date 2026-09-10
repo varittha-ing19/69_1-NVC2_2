442eb76e0943618df5c3b9ee4412e21741ba38f5
nammon <varitthasriraksa62@gmail.com>
2026-09-10 14:14:23 +0700

    22

---
+++ 059-varittha/job06.py
@@ -0,0 +1,17 @@
+number = 0
+
+while number <=12:
+    print("Number is:", number * 3)
+    number += 1
+    
+number = 0
+
+while number <=12:
+    print("Number is:", number * 4)
+    number += 1
+    
+number = 0
+
+while number <=12:
+    print("Number is:", number * 5)
+    number += 1
